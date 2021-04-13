e421c609080203f89043212d47b1606138d4bbf5
Vojtech Jelinek <vojta-pc@pop-os.localdomain>
2021-04-13 11:19:37 +0200

    added build

---
+++ build/precache-manifest.5c549ec6c680e21914b5c08965e39b51.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75e87536cda10b3c9948c4224b05583",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e658a97f2afff8353d66",
+    "url": "/static/css/main.60cb3bd2.chunk.css"
+  },
+  {
+    "revision": "b7488c6fed181e6ad5a7",
+    "url": "/static/js/2.ae44e7b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ae44e7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e658a97f2afff8353d66",
+    "url": "/static/js/main.4e4d71a9.chunk.js"
+  },
+  {
+    "revision": "bff8de1f8c8ea1c05aaf",
+    "url": "/static/js/runtime-main.28c11e25.js"
+  },
+  {
+    "revision": "9c801265aa180eb10124eab9d9f1aa95",
+    "url": "/static/media/ice.9c801265.jpg"
+  },
+  {
+    "revision": "e08b6936b9a660e8d9a6a57edeb916c4",
+    "url": "/static/media/mud.e08b6936.jpeg"
+  },
+  {
+    "revision": "41a08a6607b2f7ecacb6dd45722a8ca6",
+    "url": "/static/media/sand.41a08a66.jpeg"
+  },
+  {
+    "revision": "3e30ddae4667cf9c65f78701f245d718",
+    "url": "/static/media/snowflake.3e30ddae.jpeg"
+  }
+]);
